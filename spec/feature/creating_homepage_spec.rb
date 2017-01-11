a25d78bc762e2356a152d0e764bda023d23f5244
Konrad Pason <konrad.pason17@gmail.com>
2017-01-11 13:51:18 +0100

    Create home page spec

---
+++ spec/feature/creating_homepage_spec.rb
@@ -0,0 +1,13 @@
+require "rails_helper"
+
+RSpec.feature "Creating Home Page" do
+	
+	scenario "User visits homepage" do
+		visit "/"
+
+		expect(page).to have_link("Home")
+		expect(page).to have_link("Athletes Den")
+		expect(page).to have_content("Workout Lounge!")
+		expect(page).to have_content("Show off your workout")
+	end
+end
