bcfbbe058050fb3a4606aecceb4d5302cabc4992
Konrad Pason <konrad.pason17@gmail.com>
2017-01-20 15:34:49 +0100

    Add jquery ui datepicker

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,9 @@
 // about supported directives.
 //
 //= require jquery
+//= require jquery-ui
 //= require jquery_ujs
 //= require bootstrap-sprockets
-//= require turbolinks
+// require turbolinks
 //= require_tree .
 
